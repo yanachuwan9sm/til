5ca51929c9e0710da1ca34711edfeed64134d18f
yanachuwan9sm <gurem919@icloud.com>
2023-07-14 18:37:28 +0900

    typescript化をとりあえずした

--- diff-summary.ts
+++ diff-summary.ts
@@ -1,9 +1,9 @@
-const { exec } = require('child_process');
-const { format } = require('date-fns');
-const { writeFile } = require('fs');
-const path = require('path');
+import { exec } from 'child_process';
+import { format } from 'date-fns';
+import { writeFile } from 'fs';
+import path from 'path';
 
-function getStagedDiff() {
+function getStagedDiff(): Promise<string[]> {
   return new Promise((resolve, reject) => {
     exec('git diff --cached --name-only', (error, stdout) => {
       if (error) {
@@ -15,11 +15,11 @@ function getStagedDiff() {
   });
 }
 
-function getFileContent(fileName) {
+function getFileContent(fileName: string): Promise<string> {
   return new Promise((resolve, reject) => {
     exec(`git show :${fileName}`, (error, stdout) => {
       if (error) {
-        console.error(`Failed to get content for file ${filename}:`, error);
+        console.error(`Failed to get content for file ${fileName}:`, error);
         reject(error);
       }
       resolve(stdout);
@@ -27,10 +27,11 @@ function getFileContent(fileName) {
   });
 }
 
-function saveSummery(content) {
+function saveSummery(content: string) {
   const today = format(new Date(), 'yyyy-MM-dd');
   const fileName = `${today}.md`;
 
+  const __dirname = new URL('.', import.meta.url).pathname;
   const filePath = path.join(__dirname, 'summary', fileName);
 
   writeFile(filePath, content, (err) => {
@@ -45,7 +46,7 @@ function saveSummery(content) {
 (async function () {
   const stagedFiles = await getStagedDiff();
 
-  const promiseFuncs = [];
+  const promiseFuncs: (() => Promise<string>)[] = [];
 
   stagedFiles.forEach(async (filename) => {
     if (!filename.replaceAll('"', '').endsWith('.md')) return;
